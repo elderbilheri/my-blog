31af658614534c68d226f8f328840a16c170337c
Elder Bilheri <elderbilheri@gmail.com>
2023-03-23 19:05:40 -0300

    att component lefPanel

--- src/app/components/left-panel/left-panel.component.ts
+++ src/app/components/left-panel/left-panel.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './left-panel.component.html',
   styleUrls: ['./left-panel.component.scss'],
 })
-export class LeftPanelComponent {}
+export class LeftPanelComponent {
+  name: string = 'Elder Bilheri';
+
+  //icones
+  feedIcon = '../assets/images/icons/feed2.svg';
+  followIcon = '../assets/images/icons/follow.svg';
+  searchIcon = '../assets/images/icons/search.svg';
+  statisticsIcon = '../assets/images/icons/statistics.svg';
+  helpIcon = '../assets/images/icons/help.svg';
+}
